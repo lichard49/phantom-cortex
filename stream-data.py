7119590788f4c7199e45b199d8b00c4f6e222b45
tomaitagaki <tomaitagaki@gmail.com>
2021-11-01 15:19:51 -0700

    fft/timeseries

--- stream-data.py
+++ stream-data.py
@@ -28,7 +28,7 @@ class Graph:
         self.board_shim = board_shim
         self.exg_channels = BoardShim.get_exg_channels(self.board_id)
         self.sampling_rate = BoardShim.get_sampling_rate(self.board_id)
-        self.update_speed_ms = 50
+        self.update_speed_ms = 10
         self.window_size = 4
         self.num_points = self.window_size * self.sampling_rate
 
@@ -36,27 +36,73 @@ class Graph:
         self.win = pg.GraphicsWindow(title='BrainFlow Plot',size=(800, 600))
 
         self._init_timeseries()
+        self._init_fft()
 
         timer = QtCore.QTimer()
-        timer.timeout.connect(self.update)
+        timer.timeout.connect(self.updateFFT)
         timer.start(self.update_speed_ms)
         QtGui.QApplication.instance().exec_()
-
 
     def _init_timeseries(self):
         self.plots = list()
         self.curves = list()
+        self.ranges = list() # tuples of min / max
+        self.allrange = [-150, 150] # [-2**12, 2**12]
         for i in range(len(self.exg_channels)):
             p = self.win.addPlot(row=i,col=0)
-            p.showAxis('left', False)
+            p.showAxis('left')
             p.setMenuEnabled('left', False)
-            p.showAxis('bottom', False)
+            p.showAxis('bottom')
             p.setMenuEnabled('bottom', False)
             if i == 0:
                 p.setTitle('TimeSeries Plot')
             self.plots.append(p)
+            # self.ranges.append([-2**12, 2**12])
+            
             curve = p.plot()
             self.curves.append(curve)
+
+    def _init_fft(self):
+        self.plots = list()
+        self.curves = list()
+
+        for i in range(len(self.exg_channels)):
+            p = self.win.addPlot(row=i,col=0)
+            p.showAxis('left')
+            p.setMenuEnabled('left', False)
+            p.showAxis('bottom')
+            p.setMenuEnabled('bottom', False)
+            if i == 0:
+                p.setTitle('FFT Plot')
+            # p.setYRange(self.allrange[0], self.allrange[1])
+            self.plots.append(p)
+            curve = p.plot()
+            self.curves.append(curve)
+    
+
+    def updateFFT(self):
+        data = self.board_shim.get_current_board_data(self.num_points) # 8 ecg channels, each num pts long
+        avg_bands = [0, 0, 0, 0, 0]
+        for count, channel in enumerate(self.exg_channels):
+            # plot timeseries
+            DataFilter.detrend(data[channel], DetrendOperations.CONSTANT.value)
+            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 50.0, 4.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 60.0, 4.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            nfft = DataFilter.get_nearest_power_of_two(self.sampling_rate)
+            psd = DataFilter.get_psd_welch(data[channel], nfft, nfft // 2, self.sampling_rate,
+                                   WindowFunctions.BLACKMAN_HARRIS.value)
+            # self.plots[count].setYRange(self.fftrange[0], self.fftrange[1])
+
+            self.curves[count].setData(psd[0])
+            
+        self.app.processEvents()
+
 
     def update(self):
         data = self.board_shim.get_current_board_data(self.num_points)
@@ -72,10 +118,24 @@ class Graph:
                                         FilterTypes.BUTTERWORTH.value, 0)
             DataFilter.perform_bandstop(data[channel], self.sampling_rate, 60.0, 4.0, 2,
                                         FilterTypes.BUTTERWORTH.value, 0)
+            # update ylim based on min, max values --> experiencing latency? issues in update
+            # if len(data[channel]) >= self.num_points:
+            #     if min(data[channel]) < self.ranges[count][0]:
+            #         self.ranges[count][0] = min(data[channel])
+            #     if max(data[channel]) > self.ranges[count][1]:
+            #         self.ranges[count][1] = max(data[channel])
+            # self.plots[count].setYRange(self.ranges[count][0], self.ranges[count][1])
+            # if self.ranges[count][1] > self.allrange[1]:
+            #     self.allrange[1] = self.ranges[count][1]
+            #     self.plots[count].setYRange(self.allrange[0], self.allrange[1])
+            # if self.ranges[count][0] > self.allrange[0]:
+            #     self.allrange[0] = self.ranges[count][0]
+            #     self.plots[count].setYRange(self.allrange[0], self.allrange[1])
+            self.plots[count].setYRange(self.allrange[0], self.allrange[1])
+
             self.curves[count].setData(data[channel].tolist())
-
+            
         self.app.processEvents()
-
 
 def main():
     BoardShim.enable_dev_board_logger()
